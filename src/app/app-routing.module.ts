fb545a2ca73bc54edc3d48547bc51c15cf29fda1
chivalrous <tony@starkindustries>
2022-05-09 18:54:23 +0530

    added routing and initial components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CoinDetailsComponent } from './coin-details/coin-details.component';
+import { CoinsComponent } from './coins/coins.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path:'coins' , component: CoinsComponent},
+  { path:'coinDetail', component: CoinDetailsComponent},
+  { path:'**', redirectTo: '/coins'}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,31 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HeaderComponent } from './shared/header/header.component';
+import { FooterComponent } from './shared/footer/footer.component';
+import { CoinsComponent } from './coins/coins.component';
+import { CoinDetailsComponent } from './coin-details/coin-details.component';
+
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatSelectModule} from '@angular/material/select';
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    CoinsComponent,
+    CoinDetailsComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatSelectModule
   ],
   providers: [],
   bootstrap: [AppComponent]
